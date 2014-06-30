487a0824ad1dec81ac316f6ac5eeb3123cfa5e3e
Nick Meehan <nnmeehan@gmail.com>
2014-06-29 22:28:19 -0700

    Added Rails skeleton for portfolio site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioSite::Application.config.secret_token = '252ff58af2187e6f81c10c2b2255f341d88edd18e3bb4120a4dbc4354be29522e4596f5145823f80338a2d2991fdcb1e22ec4839c69ed0b499fa97c8ef27b81c'
