7b419133aeafc75f7e6a92ed4e9614a0b4ae8d34
Nick Meehan <nnmeehan@gmail.com>
2014-07-14 14:57:39 -0600

    Adding attr_accessible to both Project and Technology to allow for mass assigning attributes in rails console.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  attr_accessible :name, :description, :image_path, :url
   has_many :project_technologies
   has_many :technologies, through: :project_technologies
 end
--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,4 +1,5 @@
 class Technology < ActiveRecord::Base
+  attr_accessible :name, :image_path
   has_many :project_technologies
   has_many :projects, through: :project_technologies
 end
